2f8d552c3019e8e522552a5f315fb4ce4f27d10c
Matheus <matheuspferreira96@gmail.com>
2020-02-09 18:44:28 -0300

    new file createProduct

---
+++ src/createProduct.vue
@@ -0,0 +1,68 @@
+<template>
+  <view class="container">
+    <view class="header">
+      <text class="txt-header">{{txtHeader}}</text>
+    </view>
+    <view class="divider"></view>
+    <view class="body">
+      <text class="txt-body">{{txtName}}</text>
+      <text class="txt-body">{{txtPrice}}</text>
+      <text class="txt-body">{{txtAmount}}</text>
+    </view>
+    <view class="footer">
+      <button class="footer-button" title="Cadastrar"/>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      txtHeader: "Cadastro de Produtos",
+      txtName: "Nome:",
+      txtPrice: "Preço:",
+      txtAmount: "Quantidade:"
+    }
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  background-color: #DFDFDF;
+  width: 100%;
+  height: 100%;
+  flex: 1;
+}
+.header{
+  margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.txt-header{
+  color: black;
+  font-size: 30px;
+  font-family: sans-serif;
+  font-weight: bold;
+}
+.divider{
+  margin: 20px;
+  width: 100%;
+  border-color: #707070;
+}
+.body{
+  margin-left: 10px;
+}
+.txt-body{
+  font-size: 20px;
+  color: black;
+  margin: 3px;
+}
+
+.footer{
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+</style>
